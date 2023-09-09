0f3f438b0641b72e214be4d35d927abe129dcb32
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-09-08 21:45:05 -0300

    chore: enabling cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoriesController } from './categories/categories.controller';
+import { CategoriesService } from './categories/categories.service';
+import { PrismaService } from './prisma.service';
+import { ProductsService } from './products/products.service';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CategoriesController, ProductsController],
+  providers: [PrismaService, CategoriesService, ProductsService],
 })
 export class AppModule {}
